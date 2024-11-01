f7cb2550302f80a2d3912ee95ad5ff3c40bc6504
Ben Joshua Preuß <bp277981@fh-muenster.de>
2024-11-01 09:04:23 +0100

    some UI

--- src/UI.py
+++ src/UI.py
@@ -1,19 +1,45 @@
 import tkinter as tk
 import cards
 
+# window stuff
+
 window = tk.Tk()
-window.title("Anki clone")
+window.title("Flashcards")
+window.geometry("800x600")
 
 def button_press(event):
     print("button was pressed")
     #cards.addCard()
 
-addCardButton = tk.Button(text="Add card")
-showAllCardsButton = tk.Button(text="show Cards")
 
-addCardButton.bind("<Button-1>", button_press)
+def addCardPopUp():
+    popup = tk.Toplevel()
+    popup.title("new card")
+    popup.geometry("400x300")
+
+    leftsideLabel = tk.Label(popup,text="Leftside")
+    leftsideLabel.pack(pady=5)
+    leftSideEntry = tk.Entry(popup)
+    leftSideEntry.pack(pady=5)
+
+    rightSideLabel = tk.Label(popup,text="Rightside")
+    rightSideLabel.pack(pady=5)
+    rightSideEntry = tk.Entry(popup)
+    rightSideEntry.pack(pady=10)
+
+
+
+    closeButton = tk.Button(popup, text="close", command=popup.destroy)
+    closeButton.pack(pady=10)
+
+
+                            
+
+addCardButton = tk.Button( window, text="Add card", command=addCardPopUp)
+
 addCardButton.pack()
 
+showAllCardsButton = tk.Button(text="show Cards")
 showAllCardsButton.bind("<Button-1>", button_press)
 showAllCardsButton.pack()
 
